3bb6d552437690b8d9bcd05ac4fef5596110c6c0
chengxiaoqin <13225077596@163.com>
2019-04-16 19:58:57 +0800

    第一次提交

--- src/javabean/MyTools.java
+++ src/javabean/MyTools.java
@@ -4,6 +4,7 @@ import java.io.UnsupportedEncodingException;
 public class MyTools {
 	public static String toChinese(String str){
 		if(str==null) str="";
+		int a=0;
 		try{
 			str=new String(str.getBytes("ISO-8859-1"),"gb2312");
 		}
